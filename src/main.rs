8f12a4a20bafba659ddeff707584434a275e3e11
Arturo Cuya <arturocuya099@gmail.com>
2023-04-18 21:25:50 -0500

    implement health check and dummy subscriptions endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{
+    web,
+    get,
+    post,
+    App,
+    HttpServer,
+    Responder,
+    HttpResponse
+};
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    username: String
+}
+
+#[get("/health_check")]
+async fn health_check() -> impl Responder {
+    return HttpResponse::Ok();
+}
+
+// Continue in 3.8.4 Database Setup
+#[post("/subscriptions")]
+async fn subscribe(form: web::Form<FormData>) -> impl Responder {
+    return format!("Hello {}!", form.username);
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(health_check)
+            .service(subscribe)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
